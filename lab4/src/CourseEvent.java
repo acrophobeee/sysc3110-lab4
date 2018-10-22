352bf4cbdc57a3774b35412dfbcabfa1f2d3752c
nickc <nickc@172.17.147.206>
2018-10-22 19:15:44 -0400

    unchanged

---
+++ lab4/src/CourseEvent.java
@@ -0,0 +1,12 @@
+import java.util.EventObject;
+import java.util.Date;
+
+public class CourseEvent extends EventObject{
+	private Date midtermDate;
+	
+	public CourseEvent(Object arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
